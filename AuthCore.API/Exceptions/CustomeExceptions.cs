f0442a8effc82a6cb1f1ad20f59f4425c2a555c2
abdelrahman-kamel-elgendy <abdelrahman.kamel.elgendy@gmail.com>
2026-03-10 16:54:19 +0200

    update exceptions

--- AuthCore.API/Exceptions/CustomeExceptions.cs
+++ AuthCore.API/Exceptions/CustomeExceptions.cs
@@ -8,7 +8,6 @@ public class UnauthorizedException(string message = "You are not authorized to a
 public class ConflictException(string message, string? details = null) : ApiException(message, HttpStatusCode.Conflict, details) { }
 public class NotFoundException(string resource, string key) : ApiException($"Resource '{resource}' with identifier '{key}' was not found.", HttpStatusCode.NotFound) { }
 public class BadRequestException(string message, string? details = null) : ApiException(message, HttpStatusCode.BadRequest, details) { }
-public class NotImplementedException(string message = "functionality is planned but not yet implemented.", string? details = null) : ApiException(message, HttpStatusCode.NotImplemented, details) { }
 public class ValidationException(IDictionary<string, string[]> errors) : ApiException("One or more validation errors occurred.", HttpStatusCode.BadRequest)
 {
     public IDictionary<string, string[]> Errors { get; } = errors;
